3b8f2a4f0e302c8ac681f48e5e24d2836e388e20
Blandina Beauty Luyanda Sibanda <blsiband@c5r5s1.wethinkcode.co.za>
2020-07-16 13:43:17 +0200

    tried

--- ex02/ft_iterative_power.c
+++ ex02/ft_iterative_power.c
@@ -6,18 +6,27 @@
 /*   By: blsiband <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/14 12:20:50 by blsiband          #+#    #+#             */
-/*   Updated: 2020/07/15 10:02:45 by blsiband         ###   ########.fr       */
+/*   Updated: 2020/07/16 13:40:08 by blsiband         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 
 int  ft_iterative_power(int nb, int power);
-{
-	if(power == 0)
-		return 1;
-	else 
-		return nb * power(nb, power -1);
-}
 
+int main(void) {
+
+	int power;
+	printf("Enter a number that  returns the value of a power applied in less than two seconds");
+	scanf("%d", &power);
+	printf("The number %d = %d \n", power, ft_iterative_power(nb, power));
+
+			int i;
+			int product = -1;
+			while(i = power,i > -1, i--) {
+			product = power	-1;
+		return 0;
+		}
+		}
 	
