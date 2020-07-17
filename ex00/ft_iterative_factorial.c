2fb097a0a181a106dbf420d8282949d65c902193
Blandina Beauty Luyanda Sibanda <blsiband@c5r5s1.wethinkcode.co.za>
2020-07-17 12:51:56 +0200

    done

--- ex00/ft_iterative_factorial.c
+++ ex00/ft_iterative_factorial.c
@@ -6,28 +6,26 @@
 /*   By: blsiband <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/14 11:54:35 by blsiband          #+#    #+#             */
-/*   Updated: 2020/07/16 10:57:31 by blsiband         ###   ########.fr       */
+/*   Updated: 2020/07/17 12:44:58 by blsiband         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
-int ft_iterative_factorial(int nb);
+int  ft_iterative_factorial(int nb) 
+{
+	int i;
+	int product;
 
-int main(void) {
-	int nb;
-	printf("Enter a number and must return in less than two seconds");
-	scanf("%d!", &nb);
-	printf("The number %d! = %d \n", nb, ft_iterative_factorial(nb));
-	
-} 
+	if(nb < 0 || nb > 12)
+		return 0;
 
-int  ft_iterative_factorial(int nb) {
-	int i = 5;
-	int product = 1;
-	while( i=nb, i--) {
-
-		product = product * i;
+	i = 2;
+	product = 1;
+	while( i <=nb)
+	{
+		product *= i;
+		++i;
 	}
 
 	return product;
